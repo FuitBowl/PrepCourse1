e3b092078e9caf557eec50bae617c5e9d2e2ade3
ERomero <136257677+FrutaGubby@users.noreply.github.com>
2024-02-25 12:34:11 -0600

    Objetos del 08 al 21

--- C8 Objects + Callbacks/objects/ejercicios/14.js
+++ C8 Objects + Callbacks/objects/ejercicios/14.js
@@ -2,6 +2,13 @@ function tieneEmail(objetoUsuario) {
   // Verifica si el "objetoUsuario", en su propiedad "email", posee un valor definido.
   // En ese caso, retorna true. Caso contrario, false.
   // Tu código:
+ for (email in objetoUsuario) {
+  if (objetoUsuario.email == undefined) {
+  return false;
+  } else {
+    return true
+  }
+ }
 }
 
 module.exports = tieneEmail;
--- C8 Objects + Callbacks/objects/ejercicios/20.js
+++ C8 Objects + Callbacks/objects/ejercicios/20.js
@@ -4,6 +4,6 @@ function sumarLikesDeUsuario(objetoUsuario) {
   // Cada post posee una propiedad llamada "likes". Esta propiedad es un número.
   // Debes sumar los likes de todos los post y retornar el resultado.
   // Tu código:
+  return objetoUsuario.posts.reduce((num, post) => num + post.likes, 0);
 }
-
 module.exports = sumarLikesDeUsuario;
